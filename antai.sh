9708a66b136adda00c2f4c32d3d4b8dacddc3e83
Antai Okon-Otoyo <aokonotoyo@yahoo.com>
2024-12-28 09:57:35 +0100

    First Assignment Completed for Cloud Engineering Track

---
+++ antai.sh
@@ -0,0 +1,27 @@
+who_am_i() {
+	echo "Hello! My full name is Antai Okon-Otoyo"
+}
+
+bio() {
+	echo "I am a technical support engineer in a global fintech organisation with HQ based in Nigeria and I'm working on transitioning to cloud engineering. I'm currently enrolled at AltSchool Africa (StudentID: ALT/SOE/024/0530) with the aim of achieving that transition. 
+    I hold a Bachelors degree in Computer Engineering from Covenant University in Ota, Ogun State, Nigeria."
+}
+
+year_in_tech() {
+	echo "I have been in tech for 7 years. 1 year as a Junior Software Developer and 6 years as a Support Engineer"
+}
+
+dream_job() {
+	echo "My dream job is to work as a cloud engineer in a reputable company that has an aligning vision with mine and its sight set on growth and value contribution to the society"
+}
+
+goals() {
+	echo "My goal in AltSchool is to graduate as a skilled cloud engineer proficient in AWS, Azure and Google Cloud platforms and services while attaining certifications to back them up."
+    echo "Currently, I've been able to write and pass the AWS CCP already and plan to do the same for AWS SAA and AWS Developer Associate before the end of the first quarter of 2025."
+    }
+
+who_am_i
+bio
+year_in_tech
+dream_job
+goals
